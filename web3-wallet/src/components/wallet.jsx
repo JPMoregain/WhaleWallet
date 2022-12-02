4fd75189ba536343b27f5dc587a5ebff839b9a66
JP Moregain <j.p.moregain15@gmail.com>
2022-12-02 10:27:07 -0500

    Configure env files, update readme, create wallet component

---
+++ web3-wallet/src/components/wallet.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import Web3 from 'web3';
+
+const address = import.meta.env.VITE_WALLET_ADDRESS;
+const privateKey = import.meta.env.VITE_PRIVATE_KEY;
+const rpcEndpoint = import.meta.env.VITE_RPC_ENDPOINT;
+
+function Wallet() {
+  return <div>wallet</div>;
+}
+
+export default Wallet;
